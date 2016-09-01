b625dbb1ae66427006f84b590b750e2c913fb4f5
Bohao Chen <bohaoc@curacloudcorp.com>
2016-09-01 19:14:55 +0800

    update version to 0.3

--- src/script.js
+++ src/script.js
@@ -1,11 +1,17 @@
 function showFullDate() {
     var arr = document.getElementsByClassName("relativetime");
     for (var i = 0; i < arr.length; i++) {
-        arr[i].childNodes[0].nodeValue = arr[i].getAttribute("title");
+        if (arr[i].childNodes[0].nodeValue.indexOf("at") != -1) {
+            console.log(arr[i].childNodes[0].nodeValue.indexOf("at"));
+            console.log(arr[i].childNodes[0].nodeValue);
+            arr[i].childNodes[0].nodeValue = arr[i].getAttribute("title");
+        }
     }
     var arr2 = document.getElementsByClassName("relativetime-clean");
     for (var i = 0; i < arr2.length; i++) {
-        arr2[i].childNodes[0].nodeValue = arr2[i].getAttribute("title");
+        if (arr2[i].childNodes[0].nodeValue.indexOf("at") != -1) {
+            arr2[i].childNodes[0].nodeValue = arr2[i].getAttribute("title");
+        }
     }
 }
 
